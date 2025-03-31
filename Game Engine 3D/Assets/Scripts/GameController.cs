c882a59d5989a02daa782bb3284985c0d4bdfa70
JosueRafP <josue.petri.senac@gmail.com>
2025-03-31 16:38:50 -0300

    Importação de crosshair e outras melhorias no script'

---
+++ Game Engine 3D/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    public UnityEvent OnSniperMode;
+    public UnityEvent OnRifleMode;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
